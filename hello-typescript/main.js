8cda385f5c1acd4e27bf7d77a0f639b3bd2e7831
Saulus Ribeiro <bruno.neves@partners.mv.com.br>
2022-05-09 15:01:04 -0300

    First Commit

---
+++ hello-typescript/main.js
@@ -0,0 +1,17 @@
+var minhavariavel = 'Minha Variavel Typescript';
+function minhaFuncao(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) { return valor * 2; });
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.adicao = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
---
+++ hello-typescript/main.ts
@@ -0,0 +1,18 @@
+var minhavariavel = 'Minha Variavel Typescript';
+
+function minhaFuncao(x,y){
+    return x + y;
+}
+let num=2;
+const PI=3.14;
+
+var numeros = [1,2,3];
+numeros.map(function (valor){return valor * 2;});
+
+numeros.map( valor => valor * 2);
+
+class Matematica {
+
+    adicao(x,y){
+        return x+y;
+}}
